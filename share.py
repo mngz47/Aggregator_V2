4556a773f6100667cefb6496850ea0fee3439132
Mngz <mngz636@gmail.com>
2020-12-17 13:39:35 +0200

    Update share.py

--- share.py
+++ share.py
@@ -1,11 +1,11 @@
 
 import requests
 
-import FaceBook
+import facebook
 
 class share():
 
     def send(self,title,text,link):
-        ff = FaceBook()
+        ff = facebook()
         ff.__init__('app_id','app_secrete','fp_page_id')
         return ("<br>Post On FaceBook: "+ff.post_to_fb(title+text,link))
