d9659ca116a3db6ff4d5a694bc9bdd905002914e
Mngz <mngz636@gmail.com>
2020-12-17 14:16:58 +0200

    Update share.py

--- share.py
+++ share.py
@@ -7,5 +7,5 @@ class share():
 
     def send(self,title,text,link):
         ff = facebook()
-        ff.__init__('app_id','app_secrete','fp_page_id')
+        ff.__init__('838292889616021','eb485dec6fac405b62e666929f03d272','195349390839130')
         return ("<br>Post On FaceBook: "+ff.post_to_fb(title+text,link))
