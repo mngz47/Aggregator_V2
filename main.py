f962de05f31dd144ba67fa0960af57fbdf8c28cf
Mngz <mngz636@gmail.com>
2022-01-14 15:57:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ class main:
 if __name__ == "__main__":
      app.debug = False
      port = int(os.environ.get('PORT', 33507))
-     waitress.serve(app, port=port)
+     waitress.server(app, port=port)
